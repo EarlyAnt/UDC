083b1a91f5c0590967d4c36ada86d71916586066
54763755@qq.com <54763755@qq.com>
2021-08-18 17:10:33 +0800

    1.想在android设备的根目录下创建子目录，失败，即使动态申请了权限也没有用。

--- udc/lib/file_util.dart
+++ udc/lib/file_util.dart
@@ -1,11 +1,65 @@
 import 'dart:io';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Storage {
+  Future printDirectories() async {
+    Directory directory;
+    directory = await getApplicationDocumentsDirectory();
+    print("getApplicationDocumentsDirectory: ${directory.path}");
+    directory = await getApplicationSupportDirectory();
+    print("getApplicationSupportDirectory: ${directory.path}");
+    // path = await getDownloadsDirectory();
+    // print("getDownloadsDirectory: ${path.path}");
+    directory = await getTemporaryDirectory();
+    print("getTemporaryDirectory: ${directory.path}");
+    directory = await getExternalStorageDirectory();
+    print("getExternalStorageDirectory: ${directory.path}");
+    // path = await getLibraryDirectory();
+    // print("getLibraryDirectory: ${path.path}");
+    directory = await getTemporaryDirectory();
+    print("getTemporaryDirectory: ${directory.path}");
+
+    List<Directory> directories = await getExternalStorageDirectories();
+    for (var dir in directories) {
+      print("ExternalStorageDirectories: ${dir.path}\n");
+    }
+  }
+
+  Future requestPermissions() async {
+    Permission permission = Permission.storage;
+    //权限的状态
+    PermissionStatus status = await permission.status;
+
+    if (status.isUndetermined) {
+      //从未申请过
+      permission.request();
+    } else if (status.isDenied) {
+      //第一次申请用户拒绝
+      permission.request();
+    } else if (status.isPermanentlyDenied) {
+      //用户点击了 拒绝且不再提示
+      permission.request();
+    } else {
+      //权限通过
+      print("permission granted");
+    }
+  }
+
   Future<String> get _localPath async {
-    final _directory = await getExternalStorageDirectory();
-    return _directory.path;
+    final Directory packageDirectory = await getExternalStorageDirectory();
+    final Directory customDirectory =
+        Directory("${packageDirectory.parent.path}/UDC/");
+
+    bool existed = await customDirectory.exists();
+    if (!existed) {
+      await requestPermissions();
+      await customDirectory.create();
+    }
+
+    print("<><Storage._localPath>file path: ${customDirectory.path}");
+    return customDirectory.path;
   }
 
   Future<File> get _localFile async {
