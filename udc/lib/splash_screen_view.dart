ceefd32864ec04f72e3ebe7a60942ad2108090af
54763755@qq.com <54763755@qq.com>
2021-09-22 11:30:42 +0800

    1.提升版本至1.0.10(10)。

--- udc/lib/splash_screen_view.dart
+++ udc/lib/splash_screen_view.dart
@@ -72,7 +72,7 @@ class _SplashScreenViewState extends State<SplashScreenView>
               child: Visibility(
                 visible: _fadeOver,
                 child: Text(
-                  "v1.0.9",
+                  "v1.0.10",
                   style: TextStyle(fontSize: 10, color: Colors.grey.shade500),
                 ),
               ),
