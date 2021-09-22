9bd5d002de88e67e0f9d71f4388d31dd7f109a45
54763755@qq.com <54763755@qq.com>
2021-09-22 15:01:37 +0800

    1.提升版本至2.0.0(11)。

---
+++ udc/doc/pull.bat
@@ -0,0 +1,3 @@
+adb pull "/sdcard/Android/data/com.bowhead.udc/UDC/user_data_2021-09-22.csv" "user_data_2021-09-22.csv"
+
+pause
--- udc/lib/splash_screen_view.dart
+++ udc/lib/splash_screen_view.dart
@@ -72,7 +72,7 @@ class _SplashScreenViewState extends State<SplashScreenView>
               child: Visibility(
                 visible: _fadeOver,
                 child: Text(
-                  "v1.0.10",
+                  "v2.0.0",
                   style: TextStyle(fontSize: 10, color: Colors.grey.shade500),
                 ),
               ),
